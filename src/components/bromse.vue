1eb85bdcc96fbf6244bcab225d4214d9da742e61
SanLucos <luc.dumont.1985@gmail.com>
2020-05-23 00:10:32 +0200

    Add libraries selection list

--- src/components/bromse.vue
+++ src/components/bromse.vue
@@ -1,16 +1,33 @@
 <template>
-    <div class="control has-icons-right">
-      <input 
-        input class="input is-rounded" 
-        type="text"
-        v-model="request"
-        :placeholder="$t('placeholder')"
-        @keydown.enter.exact.prevent
-        @keyup.enter.exact="launchRequests"
-      />
-        <span class="icon is-right">
-          <img src="@/assets/search.svg"/>
-        </span>
+    <div>
+      <div class="control has-icons-right">
+        <input 
+          input class="input is-rounded" 
+          type="text"
+          v-model="request"
+          :placeholder="$t('placeholder')"
+          @keydown.enter.exact.prevent
+          @keyup.enter.exact="launchRequests"
+        />
+          <span class="icon is-right">
+            <img src="@/assets/search.svg"/>
+          </span>
+      </div>
+
+      <div class="container has-text-centered">Votre recherche sera exécutée sur {{ nbSearchedSites }} sites (onglets)</div>
+
+      <div class="container" style="max-width: 300px;">
+        <ul>
+          <li v-for="engine in searchEngines" :key="engine.name">
+            <label class="checkbox">
+              <input type="checkbox" v-model="engine.search" checked="engine.search">
+                {{ engine.name }}
+            </label>
+            
+          </li>
+        </ul>
+      </div>
+
     </div>
 </template>
 
@@ -28,9 +45,24 @@ export default {
       searchEngines: require("../engines.json").map(engine=>{
         return{
           ...engine,
-          search: true
+          //search: true
         }
       })
+    }
+
+  },
+
+  computed:{
+
+    nbSearchedSites: function(){
+      let i = 0
+      this.searchEngines.forEach(engine=>{
+        if (engine.search) {
+            i++
+          }
+      })
+
+      return i
     }
 
   },
