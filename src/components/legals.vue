305bafc682af9e84a09d39d0c6de94d6d700ad8b
SanLucos <luc.dumont.1985@gmail.com>
2020-05-28 17:41:40 +0200

    cosmetics

--- src/components/legals.vue
+++ src/components/legals.vue
@@ -1,13 +1,10 @@
 <template>
     <footer class="footer">
-    <div class="content has-text-centered">
-        <p>
-        <strong>B.R.O.M.S.E.</strong> by <a target="_blank" rel="noopener noreferrer" href="https://naept.com/">Naept</a>. The source code is licensed
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
-        </p>
-    </div>
-</footer>
+        <div class="content has-text-centered">
+            <p>
+            <strong>B.R.O.M.S.E.</strong> by <a target="_blank" rel="noopener noreferrer" href="https://naept.com/">Naept</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+            </p>
+        </div>
+    </footer>
 </template>
-
-<style scoped>
-</style>
